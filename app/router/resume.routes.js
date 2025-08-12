3ff8d2b357f284653224a6d898a8b5996129567f
armin ahari <ahariarmin@gmail.com>
2025-08-12 15:01:12 +0330

    fix: fix resume upload routing by adding missing route import

--- app/router/resume.routes.js
+++ app/router/resume.routes.js
@@ -1,6 +1,7 @@
 const multer = require("multer");
 const path = require("path");
 const express = require("express");
+const { uploadResume } = require("../http/controllers/resume.controller");
 const { verifyAccessToken } = require("../http/middlewares/user.middleware");
 const fs = require("fs");
 
